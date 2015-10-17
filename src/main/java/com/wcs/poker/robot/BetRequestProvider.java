287a47006607c6cd1467ef4b83d5fc6cb9e3edd3
Corax <komlosi.laszlo@webstar.hu>
2015-10-17 12:09:13 +0200

    Kezdőkéz kiértékelés kezdemény

--- src/main/java/com/wcs/poker/robot/BetRequestProvider.java
+++ src/main/java/com/wcs/poker/robot/BetRequestProvider.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+
  */
 package com.wcs.poker.robot;
 
@@ -17,18 +18,41 @@ public class BetRequestProvider {
 
     private GameState gameState;
     private int betRequest = 10;
-    
+
     public BetRequestProvider(GameState gameState) {
         this.gameState = gameState;
-       // init();
+        init();
     }
-    
-    private void init(){
+
+    private void init() {
         List<Card> cards = gameState.getCommunityCards();
         List<Card> ourCards = gameState.getPlayers().get(gameState.getInAction()).getHoleCards();
-    }
+        HandProvider hp = new HandProvider(ourCards);
+
+        switch (hp.handStrenght()){
+            case 1: {
+                setBetRequest(5*(gameState.getCurrentBuyIn()-gameState.getPlayers().get(gameState.getInAction()).getBet()+gameState.getMinimumRaise()));
+                break;
+            }
+            case 2:{
+                setBetRequest(2*(gameState.getCurrentBuyIn()-gameState.getPlayers().get(gameState.getInAction()).getBet()+gameState.getMinimumRaise()));
+                break;
+            }
+            case 3: break;
+            case 4: break;
+
+        }
+        
+        }
+
     
-    public int getBetRequest(){
+
+    public int getBetRequest() {
         return betRequest;
     }
+
+    public void setBetRequest(int betRequest) {
+        this.betRequest = betRequest;
+    }
+
 }
--- src/main/java/com/wcs/poker/robot/HandProvider.java
+++ src/main/java/com/wcs/poker/robot/HandProvider.java
@@ -6,6 +6,8 @@
 package com.wcs.poker.robot;
 
 import com.wcs.poker.gamestate.Card;
+import com.wcs.poker.gamestate.Rank;
+import java.util.List;
 
 /**
  *
@@ -32,11 +34,47 @@ public class HandProvider {
         this.masodik = masodik;
     }
 
-    public HandProvider(Card elso, Card masodik) {
-        this.elso = elso;
-        this.masodik = masodik;
+    public HandProvider(List<Card> ourCards) {
+        this.elso = ourCards.get(0);
+        this.masodik = ourCards.get(1);
     }
-    
-    
 
+    public int handStrenght() {
+        if (isBigCards()) {
+            return 1;
+        }
+        else
+        {
+        if (isStrongCards()){
+            return 2;
+        }
+        }
+        return 4;
+        
+    }
+
+    public boolean isBigCards() {
+        if ((elso.getRank().equals(Rank.ACE)
+                || elso.getRank().equals(Rank.KING))
+                && elso.getRank().equals(masodik.getRank())) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isStrongCards() {
+        if ((elso.getRank().equals(Rank.TEN)
+                || elso.getRank().equals(Rank.QUEEN) 
+                || elso.getRank().equals(Rank.JACK)) 
+                && elso.getRank().equals(masodik.getRank())) {
+            return true;
+        }
+        if ((elso.getRank().equals(Rank.ACE)
+                || elso.getRank().equals(Rank.KING))
+                && (masodik.getRank().equals(Rank.ACE)
+                || masodik.getRank().equals(Rank.KING))) {
+            return true;
+        }
+        return false;
+    }
 }
